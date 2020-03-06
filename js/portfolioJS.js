9533b986ca542df94a0c57cfe944d246810f5d6e
AlisonAspen <aaf425@nyu.edu>
2020-03-06 13:28:44 -0500

    added js links for index page

--- js/portfolioJS.js
+++ js/portfolioJS.js
@@ -3,5 +3,11 @@ function loadUnity() {
      window.open("unity_3D.html", "_self");
 }
 function loadPhotography() {
-     window.open("index.html");
+     window.open("photography.html", "_self");
+}
+function loadTraditional() {
+     window.open("traditional.html", "_self");
+}
+function loadModeling() {
+     window.open("modeling.html", "_self");
 }
